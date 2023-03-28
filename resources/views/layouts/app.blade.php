593c429b2e54077ca415c82465bfe626fd209063
Miguel Leite <miguelleite200leite@gmal.com>
2023-03-28 09:10:42 +0100

    🔧 config(tailwind) config tailwind on project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,9 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script src="https://cdn.tailwindcss.com"></script>
-  <script src="{{ asset("js/tailwind.js") }}"></script>
-  <link rel="stylesheet" href="{{ asset("css/global.css") }}">
+  @vite('resources/css/app.css')
   <title>@yield('title') |  AirJet </title>
 </head>
 <body>
